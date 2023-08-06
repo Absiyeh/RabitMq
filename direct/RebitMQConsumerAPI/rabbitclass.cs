f0e6b8ae363d5d19a598370e357f8ceb9282f506
Mohammad72021 <cr.7mr.a@gmail.com>
2023-08-06 11:29:48 +0330

    Add project files.

---
+++ direct/RebitMQConsumerAPI/rabbitclass.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System;
+
+namespace RebitMQConsumerAPI
+{
+    public class rabbitclass: DefaultBasicConsumer
+    {
+        private readonly IModel _channel;
+
+        public rabbitclass(IModel channel)
+        {
+            _channel = channel;
+        }
+        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> body)
+        {
+
+            var bodys = body.ToArray();
+            var message = Encoding.UTF8.GetString(bodys);
+
+            Console.WriteLine($"Product message received: {message}");
+
+            _channel.BasicAck(deliveryTag, false);
+        }
+
+    }
+}
